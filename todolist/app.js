7ed14105c13d508a148d43cc6defdc42407fec99
Amrita <71868334+Am0908@users.noreply.github.com>
2023-04-26 13:37:04 +0530

    Update app.js

--- todolist/app.js
+++ todolist/app.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongo "mongodb+srv://cluster0.hvctvpc.mongodb.net/todolistDB" --username <amrita>");
+mongoose.connect("mongosh "mongodb+srv://cluster0.hvctvpc.mongodb.net/todolistDB" --apiVersion 1 --username <amrita>");
 const itemSchema = new mongoose.Schema({
   name: String
 });
